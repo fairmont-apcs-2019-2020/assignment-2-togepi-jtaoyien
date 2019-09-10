3ec6c54c85d15663e5885852e05d2dfd94767300
21chi <21chi@crl.verisign.net>
2019-09-10 13:51:31 -0700

    Turning in

--- Togepi/src/TogepiThing.java
+++ Togepi/src/TogepiThing.java
@@ -1,14 +1,22 @@
-
 public class TogepiThing extends PokeThing {
-
-	public TogepiThing()
-	{
-		super("togepi");
+	
+	public TogepiThing() {
+		super("togepi" , "" , 0);
 	}
 	
 	public void step()
 	{
-		move(null);
+		Direction d = getRandomDirection(); 
+		setDirection(d);
+		move();
 	}
 	
+	public Direction getRandomDirection()
+	{
+		Direction d = Direction.NORTH;
+		int i = (int)(Math.random() * 8) + 1;
+		for (int j = 0; j < i; j++)
+			d = d.nextHalf();
+		return d;
+	}
 }
