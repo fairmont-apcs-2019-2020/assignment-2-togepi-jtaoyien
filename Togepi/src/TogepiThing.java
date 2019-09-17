d5332ee391d2c6a2a3297b27080f485ad870920f
21chi <21chi@1TTTSurface2.attlocal.net>
2019-09-17 13:52:36 -0700

    Turning in fixed togepithing.java

--- Togepi/src/TogepiThing.java
+++ Togepi/src/TogepiThing.java
@@ -6,17 +6,6 @@ public class TogepiThing extends PokeThing {
 	
 	public void step()
 	{
-		Direction d = getRandomDirection(); 
-		setDirection(d);
 		move();
-	}
-	
-	public Direction getRandomDirection()
-	{
-		Direction d = Direction.NORTH;
-		int i = (int)(Math.random() * 8) + 1;
-		for (int j = 0; j < i; j++)
-			d = d.nextHalf();
-		return d;
 	}
 }
